e48b1343951551e293801701afc82b0a071082bb
Floride125-art <FTuyisenge120@daviscollege.com>
2021-04-17 23:36:44 +0200

    add sign up form for user

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from .models import Post, Profile, Comment
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('image','name', 'caption')
+class SignUpForm(UserCreationForm):
+   
+
+    class Meta:
+        model = User
+        fields = ('username',  'password1', 'password2', )
