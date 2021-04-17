0b3edb319e8775dcd4085cf9e87b4db5712545f8
Floride125-art <FTuyisenge120@daviscollege.com>
2021-04-17 23:23:41 +0200

    add forms file to create forms used in my project

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
+def home(request):
+    posts = Post.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    form = PostForm(request.POST or None, files=request.FILES)      
+    if form.is_valid():
+        post=form.save(commit=False)
+        post.user = request.user.profile
+        post.save()
+        return redirect('home')
+    context = {
+        'posts': posts,
+        'form': form,
+        'users':users,
+    }
+    return render(request, 'home.html', context)
