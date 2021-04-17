e00089491f7a54b3bb69ea3925b2aabf22219e1e
Floride125-art <FTuyisenge120@daviscollege.com>
2021-04-17 23:34:14 +0200

    add a model class to add comment on user profile

--- app/models.py
+++ app/models.py
@@ -41,3 +41,20 @@ class Post(models.Model):
 
     def total_likes(self):
         return self.likes.count()
+class Follow(models.Model):
+    following = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followers = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return self.following
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.comment
+
+    class Meta:
+        ordering = ["-pk"]
