275b2277573a66ba2e4bd8fe5eb55ce03b1f5a4f
Floride125-art <FTuyisenge120@daviscollege.com>
2021-04-17 23:35:36 +0200

    add a form to help user add a post

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,6 @@
 from django import forms
+from .models import Post, Profile, Comment
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('image','name', 'caption')
