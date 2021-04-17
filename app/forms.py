ade4f9676398de842662fd4c74dca89ee6d355f1
Floride125-art <FTuyisenge120@daviscollege.com>
2021-04-17 23:38:39 +0200

    add class for comment form for user to add comment

--- app/forms.py
+++ app/forms.py
@@ -16,3 +16,12 @@ class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['profile', 'bio']
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
