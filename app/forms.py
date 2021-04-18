5877e7e80d53768566ae219d2bf044135e20dff5
Floride125-art <FTuyisenge120@daviscollege.com>
2021-04-18 22:44:23 +0200

    modified my files to remove bugs and get my app run well

--- app/forms.py
+++ app/forms.py
@@ -1,21 +1,27 @@
 from django import forms
 from .models import Post, Profile, Comment
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('image','name', 'caption')
+
 class SignUpForm(UserCreationForm):
    
 
     class Meta:
         model = User
         fields = ('username',  'password1', 'password2', )
+
 class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['profile', 'bio']
+
+
 class CommentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 # Create your models here.
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True, default='No bio')
@@ -10,11 +12,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
+
 class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
     name = models.CharField(max_length=250, blank=True)
@@ -41,12 +46,15 @@ class Post(models.Model):
 
     def total_likes(self):
         return self.likes.count()
+
 class Follow(models.Model):
     following = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
     followers = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
 
     def __str__(self):
         return self.following
+
+
 class Comment(models.Model):
     comment = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
